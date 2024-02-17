6ceb6f42e359aeb257a520d181b85bdcc13da230
jewlmils <st.jewelmilagrosa@gmail.com>
2024-02-17 09:07:19 +0800

    initial commit

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,51 @@
+class NotesController < ApplicationController
+    before_action :set_note, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @notes = Note.all
+    end
+
+    def show
+    end
+
+    def new
+        @note = Note.new
+    end
+
+    def create
+        @note = Note.new(note_params)
+        if @note.save
+            redirect_to @note
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @note.update(note_params)
+            redirect_to @note, notice: "The note has been successfully updated."
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @note.destroy
+        redirect_to root_path, notice: "The note has been successfully removed."
+    end
+
+    private
+
+    def note_params
+        params.require(:note).permit(:title, :body)
+    end
+
+    def set_note
+        @note = Note.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
+    end
+end
